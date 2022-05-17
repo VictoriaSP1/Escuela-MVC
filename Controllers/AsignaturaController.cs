3fb6d0b23936b8fcd27f8b5b9578dd8cf7676221
Victoria Solís <dual.victoria.solis@gmail.com>
2022-05-17 10:40:55 -0500

    Correción de errores en la vista "MultiAsignaturas" y en el controlador "AsignaturaController"

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -9,14 +9,33 @@ namespace Escuela_MVC.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index ()
+        public IActionResult index ()
         {
-            var Asignatura = new Asignatura {
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Programación"
+            return View(new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()}
+                        );
+        }
+        public IActionResult MultiAsignatura ()
+        {
+            var listaAsignaturas = new List<Asignatura>(){
+                            new Asignatura{Nombre="Matemáticas",
+                                UniqueId= Guid.NewGuid().ToString()
+                            } ,
+                            new Asignatura{Nombre="Educación Física",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Castellano",
+                                UniqueId= Guid.NewGuid().ToString()
+                            },
+                            new Asignatura{Nombre="Ciencias Naturales",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
+                            ,
+                            new Asignatura{Nombre="Programación",
+                                UniqueId= Guid.NewGuid().ToString()
+                            }
             };
-          
-            return View(Asignatura);
+            return View("MultiAsignatura", listaAsignaturas);
         }
     }
 }
